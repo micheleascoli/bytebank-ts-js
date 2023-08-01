27523809518bd02c1b13e07b3b2d5bd54157268e
Michele Ascoli <micheleascoli17@gmail.com>
2023-08-01 15:23:21 -0500

    Uso do Type Alias e Enums

--- src/nova-transacao-component.ts
+++ src/nova-transacao-component.ts
@@ -1,13 +1,3 @@
-let saldo = 3000;
-
-alert("Testando compilação do TS");
-
-const elementoSaldo = document.querySelector(".saldo-valor .valor") as HTMLElement;
-if(elementoSaldo != null) {
-    elementoSaldo.textContent = saldo.toString();
-}
-
-
 const elementoFormulario = document.querySelector(".block-nova-transacao form") as HTMLFormElement;
 elementoFormulario.addEventListener("submit", function(event){
     event.preventDefault();
@@ -27,13 +17,13 @@ elementoFormulario.addEventListener("submit", function(event){
     const inputValor = elementoFormulario.querySelector("#valor") as HTMLInputElement;
     const inputData = elementoFormulario.querySelector("#data") as HTMLInputElement;
 
-    let tipoTransacao: string = inputTipoTransacao.value;
+    let tipoTransacao: TipoTransacao = inputTipoTransacao.value as TipoTransacao;
     let valor: number = inputValor.valueAsNumber;
     let data: Date = new Date(inputData.value);
 
-    if(tipoTransacao == "Depósito") {
+    if(tipoTransacao == TipoTransacao.DEPOSITO) {
         saldo+= valor;
-    } else if (tipoTransacao == "Transferência" || tipoTransacao == "Pagamento de Boleto") {
+    } else if (tipoTransacao == TipoTransacao.TRANSFERENCIA || tipoTransacao == TipoTransacao.PAGAMENTO_BOLETO) {
         saldo-=valor;
     } else {
         alert("Tipo de Transação é inválido!");
@@ -42,7 +32,7 @@ elementoFormulario.addEventListener("submit", function(event){
 
     elementoSaldo.textContent = saldo.toString();
 
-    const novaTransação = {
+    const novaTransação: Transacao = {
         tipoTransacao: tipoTransacao,
         valor: valor,
         data: data
@@ -51,4 +41,3 @@ elementoFormulario.addEventListener("submit", function(event){
     console.log(novaTransação);
     elementoFormulario.reset();
 });
-
